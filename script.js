cbbca310bc90d8fb66c36392fc09435bb76d2d5d
thalyson123ui <69772569+thalyson123ui@users.noreply.github.com>
2025-06-04 11:19:07 -0300

    css e js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Active Header
+
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => { header.classList.toggle("active", window.scrollY > 0 ); });
+
+menu.onclick = () => { navbar.classList.toggle("active"); }
+window.onscroll = () => { navbar.classList.remove("active"); }
